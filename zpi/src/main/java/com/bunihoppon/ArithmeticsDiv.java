c9aaa3f3c2db7cf11211fe2c217cba51d389970e
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-18 14:18:27 +0100

    fix: restore exception handling for division by zero

--- zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
+++ zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
@@ -1,12 +1,10 @@
 package com.bunihoppon;
 
 public class ArithmeticsDiv implements IArithmeticsDiv {
-
     @Override
     public double Division(double A, double B) {
-        if (B == 0) {
-            System.out.println("Błąd: Dzielenie przez zero!");
-            return 0;
+        if (B == 0.0) {
+            throw new ArithmeticException("Division by zero");
         }
 
         return A / B;
