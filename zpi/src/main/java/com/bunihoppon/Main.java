dd4a57f8c9cb8d6ed68f344a054763f6e1f90fb3
YevhenOleinichenko <eu.oleynich@gmail.com>
2026-03-18 11:58:29 +0100

    fix println

--- zpi/src/main/java/com/bunihoppon/Main.java
+++ zpi/src/main/java/com/bunihoppon/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(
                 "BunniHopPon Andrii Bialkovskyi (253190) github: justkinou (SCRUM Master / DevOps)");
-        System.out.printlb("B1ona4y developer");
+        System.out.println("B1ona4y - developer");
     }
 }
