a96aeec95fa476d884a7bb87c44fc88b46d640bb
lostbtw <253811@edu.p.lodz.pl>
2026-03-18 11:58:41 +0100

    feat: add lostbtw role display

--- zpi/src/main/java/com/bunihoppon/Main.java
+++ zpi/src/main/java/com/bunihoppon/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(
                 "BunniHopPon Andrii Bialkovskyi (253190) github: justkinou (SCRUM Master / DevOps)");
+        System.out.println(
+            "lostbtw Tester");
     }
+
 }
