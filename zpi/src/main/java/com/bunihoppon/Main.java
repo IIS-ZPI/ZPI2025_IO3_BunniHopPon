ddf648e78ffe35e3e3dae3661fb679b211072947
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-18 11:45:27 +0100

    feat: Add initial Maven project structure and Main class

---
+++ zpi/src/main/java/com/bunihoppon/Main.java
@@ -0,0 +1,8 @@
+package com.bunihoppon;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+                "BunniHopPon Andrii Bialkovskyi (253190) github: justkinou (SCRUM Master / DevOps)");
+    }
+}
