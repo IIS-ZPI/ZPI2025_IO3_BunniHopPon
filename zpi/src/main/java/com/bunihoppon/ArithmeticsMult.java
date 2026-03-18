2c2f5dd974c473d5a3a66c53721fbbd3f7e7f8d7
YevhenOleinichenko <eu.oleynich@gmail.com>
2026-03-18 12:24:05 +0100

    feat: Add ArithmeticsMult interface and implementation

---
+++ zpi/src/main/java/com/bunihoppon/ArithmeticsMult.java
@@ -0,0 +1,10 @@
+package com.bunihoppon;
+
+public class ArithmeticsMult implements IArithmeticsMult{
+
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
+
+}
