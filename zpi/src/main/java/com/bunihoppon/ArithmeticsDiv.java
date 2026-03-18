a53a601926cb4ce0387d21600591110bb83ff5ac
Mikita Dzeviatau <9mikita@gmail.com>
2026-03-18 12:38:09 +0100

    refactor: format file

--- zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
+++ zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
@@ -1,14 +1,14 @@
 package com.bunihoppon;
 
-public class ArithmeticsDiv implements IArithmeticsDiv{
+public class ArithmeticsDiv implements IArithmeticsDiv {
 
     @Override
-    public double Division(double A, double B){
+    public double Division(double A, double B) {
         if (B == 0) {
             System.out.println("Błąd: Dzielenie przez zero!");
-        return 0;
+            return 0;
         }
-        
+
         return A / B;
     }
 }
