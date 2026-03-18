9ebee77696f7b34e04cb4a6eadd41ee7bbe6dbfe
Kacper Kazimierczak <255622@edu.p.lodz.pl>
2026-03-18 12:23:40 +0100

    fix ArithmeticsDiv implementation

--- zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
+++ zpi/src/main/java/com/bunihoppon/ArithmeticsDiv.java
@@ -4,6 +4,11 @@ public class ArithmeticsDiv implements IArithmeticsDiv{
 
     @Override
     public double Division(double A, double B){
+        if (B == 0) {
+            System.out.println("Błąd: Dzielenie przez zero!");
+        return 0;
+        }
+        
         return A / B;
     }
 }
