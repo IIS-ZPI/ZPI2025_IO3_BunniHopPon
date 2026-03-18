66676fca5d3803f3ffb0fa72dbac704732f15aee
justkinou <justkinou@proton.me>
2026-03-18 15:53:58 +0100

    comment on add package naming

--- zpi/src/main/java/com/bunihoppon/arithmetics/Add/ArithmeticsAdd.java
+++ zpi/src/main/java/com/bunihoppon/arithmetics/Add/ArithmeticsAdd.java
@@ -1,3 +1,5 @@
+// The package name should be lowercase. Also, we should
+// probably rename it to something more meaningful, like 'addition'.
 package com.bunihoppon.arithmetics.Add;
 
 public class ArithmeticsAdd implements IArithmeticsAdd {
