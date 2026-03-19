08eeb79d301b4e407d304a0c12f9046bac86297d
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-03-20 00:04:57 +0100

    add description for IArithmeticsDiv interface

--- zpi/src/main/java/com/bunihoppon/arithmetics/Div/IArithmeticsDiv.java
+++ zpi/src/main/java/com/bunihoppon/arithmetics/Div/IArithmeticsDiv.java
@@ -1,5 +1,9 @@
 package com.bunihoppon.arithmetics.Div;
 
+/// Any class implementing this interface must provide an implementation
+/// for the Division method, which takes two double values and returns
+/// the result of dividing the first value by the second.
+
 interface IArithmeticsDiv {
     double Division(double A, double B);
 }
