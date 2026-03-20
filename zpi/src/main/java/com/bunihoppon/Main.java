464f41b35ee57c57e9866f7843188cc63b221050
justkinou <justkinou@proton.me>
2026-03-21 00:30:35 +0100

    Merge pull request #19 from IIS-ZPI/feature_Lab1_Zad6/253811
    feat: implement additional negative decision test

--- zpi/src/main/java/com/bunihoppon/Main.java
+++ zpi/src/main/java/com/bunihoppon/Main.java
@@ -41,5 +41,7 @@ public class Main {
         } catch (ArithmeticException e) {
             IO.println(e);
         }
+        IO.println("=== Div test 3 ===");
+        IO.println(adiv.Division(7, -2));
     }
 }
