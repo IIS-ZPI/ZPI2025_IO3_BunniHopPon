0a5e9e9597450db4b53a426538665cc7a185a132
Nurgl1ch <101202362+B1ona4y@users.noreply.github.com>
2026-03-21 00:43:51 +0100

    Merge pull request #20 from IIS-ZPI/feature_Lab1_Zad6/252499_multTest
    Feature lab1 zad6/mult test 2

--- zpi/src/main/java/com/bunihoppon/Main.java
+++ zpi/src/main/java/com/bunihoppon/Main.java
@@ -30,7 +30,9 @@ public class Main {
         IO.println("=== Mult test ===");
         ArithmeticsMult amult = new ArithmeticsMult();
         IO.println(amult.Multiplication(10, 2));
-
+        IO.println("=== Mult test 2 ===");
+        IO.println(amult.Multiplication(10, 0));
+        // Valid division test
         IO.println("=== Div test ===");
         ArithmeticsDiv adiv = new ArithmeticsDiv();
         IO.println(adiv.Division(10, 2));
