20219aa7361caeb9f8add83521bae20bef01b875
B1ona4y <eu.oleynich@gmail.com>
2026-03-20 22:42:02 +0100

    mult comments

--- zpi/src/main/java/com/bunihoppon/arithmetics/Mult/ArithmeticsMult.java
+++ zpi/src/main/java/com/bunihoppon/arithmetics/Mult/ArithmeticsMult.java
@@ -1,7 +1,7 @@
 package com.bunihoppon.arithmetics.Mult;
 
 public class ArithmeticsMult implements IArithmeticsMult {
-
+    // Handles multiplication operations
     @Override
     public double Multiplication(double A, double B) {
         return A * B;
