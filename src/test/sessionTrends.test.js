81fb8e312a4d783c145d99ac715f2fb3f94aaab1
justkinou <justkinou@proton.me>
2026-05-16 00:16:57 +0200

    add dummy functions for session trends module
    Co-authored-by: lostbtw <bozo@>

--- src/test/sessionTrends.test.js
+++ src/test/sessionTrends.test.js
@@ -8,7 +8,7 @@ import {
   calcStdDev,
   calcCoeffOfVariation,
   calcMinMaxAvg
-} from "../sessionTrends.js";
+} from "../core/sessionTrends.js";
 
 describe("normalizeNbpResponse", () => {
   it("maps NBP response into date/rate points", () => {
